9afa7c0c38b9679c9308b70f24f65bcd897cd067
Gustavo A. Gutkoski <gustavogutkoski2609@gmail.com>
2025-08-31 16:43:18 -0300

    feat: initialize Spring Boot project

---
+++ src/main/java/com/gutkoski/streamforge/api/StreamforgeApiApplication.java
@@ -0,0 +1,13 @@
+package com.gutkoski.streamforge.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamforgeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamforgeApiApplication.class, args);
+	}
+
+}
